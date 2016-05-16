e231a8800f36aa30b2526490c3ddfdcf2d04ba1e
kobe96 <906073749@qq.com>
2016-05-16 16:51:00 +0800

    the fifth homework finnal

---
+++ calculator v2.1/calculator/calculator.cpp
@@ -0,0 +1,164 @@
+#include"calculator.h"
+
+double calculator::read(queue<string> inputfrommain)
+{
+	if (inputfrommain.front()[0] == '-')
+	{
+		strinput = { '0' };
+		input.push(strinput);
+	}
+
+	while (!inputfrommain.empty())
+	{
+		if (inputfrommain.front()[0] == '(')
+		{
+			strinput = inputfrommain.front();
+			input.push(strinput);
+			inputfrommain.pop();
+			if (inputfrommain.front()[0] == '-')
+			{
+				strinput = { '0' };
+				input.push(strinput);
+			}
+		}
+		else
+		{
+			strinput = inputfrommain.front();
+			input.push(strinput);
+			inputfrommain.pop();
+		}
+	}
+
+
+	while (!input.empty())
+	{
+		strinput = input.top();
+		if (strinput[0] >= '0'&&strinput[0] <= '9')
+		{
+			Oppositeoutput.push(strinput);
+			input.pop();
+		}
+		else if (strinput[0] == ')')
+		{
+			operators.push(strinput);
+			input.pop();
+		}
+		else if (strinput[0] == '(')
+		{
+			while (operators.top()[0] != ')')
+			{
+				Oppositeoutput.push(operators.top());
+				operators.pop();
+			}
+			operators.pop();
+			input.pop();
+		}
+		else
+		{
+			if (operators.empty())
+			{
+				operators.push(strinput);
+				input.pop();
+			}
+			else if (operators.top()[0] == ')')
+			{
+				operators.push(strinput);
+				input.pop();
+			}
+			else if (level(strinput) >= level(operators.top()))
+			{
+				operators.push(strinput);
+				input.pop();
+			}
+
+			else
+			{
+				Oppositeoutput.push(operators.top());
+				operators.pop();
+			}
+
+		}
+
+	}
+	while (!operators.empty())
+	{
+		Oppositeoutput.push(operators.top());
+		operators.pop();
+	}
+
+	while (!Oppositeoutput.empty())
+	{
+		output.push(Oppositeoutput.top());
+		Oppositeoutput.pop();
+	}
+	while (!output.empty())
+	{
+		strinput = output.top();
+		if (strinput[0] >= '0'&&strinput[0] <= '9')
+		{
+			stream << strinput;
+			stream >> number;
+			num.push(number);
+			output.pop();
+			stream.clear();
+		}
+		else
+		{
+			firstnum = num.top();
+			num.pop();
+			result = js(firstnum, num.top(), strinput);
+			num.pop();
+			num.push(result);
+			output.pop();
+		}
+	}
+	while (!operators.empty())
+	{
+		operators.pop();
+	}
+	while (!input.empty())
+	{
+		input.pop();
+	}
+	while (!Oppositeoutput.empty())
+	{
+		Oppositeoutput.pop();
+	}
+	while (!output.empty())
+	{
+		output.pop();
+	}
+	while (!inputfrommain.empty())
+	{
+		inputfrommain.pop();
+	}
+	while (!num.empty())
+	{
+		num.pop();
+	}
+
+
+	return result;
+}
+
+int calculator::level(string priority)
+{
+	if (priority[0] == '-' || priority[0] == '+')
+		return 1;
+	if (priority[0] == '/' || priority[0] == '*')
+		return 2;
+}
+
+double calculator::js(double firstnum, double secondnum, string operators)
+{
+	if (strinput[0] == '+')
+		return firstnum + secondnum;
+	if (strinput[0] == '-')
+		return firstnum - secondnum;
+	if (strinput[0] == '*')
+		return firstnum * secondnum;
+	if (strinput[0] == '/')
+		return firstnum / secondnum;
+
+	return 0.0;
+}
---
+++ calculator v2.1/calculator/main.cpp
@@ -0,0 +1,191 @@
+#include <fstream> 
+#include<iostream>
+#include<string>
+#include<queue>
+#include<stack>
+#include"Scan.h"
+#include"print.h"
+#include"calculator.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int ss=0;
+	int pdinput = 0;
+	double result;
+	int pdlenth = 0;
+	int pd(string str);
+	int pdk(string input);
+	queue<string>midsection;
+	Scan sc;
+	print pr;
+	calculator ca;
+	string input, input1, error;
+
+	if (argc == 4 && argv[1][0] == '-' && argv[1][1] == 'f')
+	{
+		ifstream in;
+		in.open(argv[2]);
+
+
+		while (!in.eof())
+		{
+			getline(in, input);
+			input.erase(input.end() - 1);
+			if (pd(input) == 0 && pdk(input) == 0 && input != "")
+			{
+				midsection = sc.ToStringQueue(input);
+				result = ca.read(midsection);
+				pr.OutputResult(result, error, argv[3]);
+			}
+			else if (input == "")
+			{
+				continue;
+			}
+			else
+			{
+				result = 0;
+				error = "error";
+				pr.OutputResult(result, error, argv[3]);
+			}
+			error = "";
+			input = "";
+			while (!midsection.empty())
+			{
+				midsection.pop();
+			}
+		}
+		return 0;
+	}
+
+	else
+	{
+		if (argc < 2)
+		{
+			pdinput = 1;
+		}
+		if (pdinput == 0)
+		{
+			if (argc == 3 && argv[1][0] == '-'&&argv[1][1] == 'a')
+			{
+				pdlenth = 1;
+				input = argv[2];
+				input.erase(input.end() - 1);
+			}
+			else
+			{
+				input = argv[1];
+				input.erase(input.end() - 1);
+			}
+		}
+		else
+		{
+			cin >> input1;
+			if (input1[0] == '-'&&input1[1] == 'a')
+			{
+				cin >> input;
+				input.erase(input.end() - 1);
+				pdlenth = 1;
+			}
+			else
+			{
+				input = input1;
+				input.erase(input.end() - 1);
+			}
+		}
+
+		if (pdlenth == 1)
+		{
+			if (pd(input) == 0 && pdk(input) == 0)
+			{
+				midsection = sc.ToStringQueue(input);
+				result = ca.read(midsection);
+				pr.PrintStringQueue(midsection);
+				cout << "= ";
+				cout << result << endl;
+
+			}
+			else
+			{
+				cout << "error" << endl;
+			}
+		}
+		if (pdlenth == 0)
+		{
+			if (pd(input) == 0 && pdk(input) == 0)
+			{
+				midsection = sc.ToStringQueue(input);
+				result = ca.read(midsection);
+				cout << result << endl;
+			}
+			else
+			{
+				cout << "error" << endl;
+			}
+		}
+		return 0;
+	}
+}
+
+int pd(string str)
+{
+	int i, pds = 0, pdd = 0;
+	for (i = 0; i < str.size(); i++)
+	{
+		if (str[i] <= '9'&&str[i] >= '0' || str[i] == '.')
+		{
+			pds++;
+			if (str[i] == '.')
+			{
+				pdd = 1;
+			}
+			if (pdd == 1 && pds > 11)
+			{
+				return 1;
+				break;
+			}
+			if (pdd == 0 && pds > 10)
+			{
+				return 1;
+				break;
+			}
+		}
+		else
+		{
+			pds = 0;
+		}
+	}
+	return 0;
+
+}
+
+int pdk(string input)
+{
+	string midsection;
+	stack<string>brackets;
+	int i;
+	for (i = 0; i < input.size(); i++)
+	{
+		if (input[i] == '(')
+		{
+			midsection = input[i];
+			brackets.push(midsection);
+			i++;
+		}
+		if (input[i] == ')')
+		{
+			if (brackets.empty())
+				return 0;
+			else
+			{
+				brackets.pop();
+				continue;
+			}
+		}
+	}
+	if (!brackets.empty())
+		return 1;
+	else
+		return 0;
+}
